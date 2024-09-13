39e40fe517bec2070fa040a74ca801e18901a8fc
saif ur rehman <saif1312687@gmail.com>
2024-09-13 06:08:08 +0500

    funtions

--- Day_4/function.js
+++ Day_4/function.js
@@ -13,14 +13,14 @@ function intro() {
 
 //  02
 
-function calcReactArea(widht,height){
+function calcReactArea(widht, height) {
 
-    return widht*height
+    return widht * height
 
 };
 
 // with prameters
-const result =calcReactArea(5,5);
+const result = calcReactArea(5, 5);
 // console.log(result);
 
 
@@ -30,18 +30,48 @@ const result =calcReactArea(5,5);
 
 //  03
 
-function introductionMe(userName , Proffesion , Age){
+function introductionMe(userName, Proffesion, Age) {
 
-console.log(`My Name is ${userName}`);
-console.log(`I am a ${Proffesion}`);
-console.log(`I am ${Age}`);
+    console.log(`My Name is ${userName}`);
+    console.log(`I am a ${Proffesion}`);
+    console.log(`I am ${Age}`);
 
 
 };
 
 
-const userName=prompt("Enter Your Name..");
-const Proffesion=prompt("Enter Your Profession..");
-const Age=prompt("Enter Your Age..");
+// const userName=prompt("Enter Your Name..");
+// const Proffesion=prompt("Enter Your Profession..");
+// const Age=prompt("Enter Your Age..");
 
-introductionMe(userName , Proffesion , Age);
+// introductionMe(userName , Proffesion , Age);
+
+
+
+
+
+
+
+//   NOTE:
+// Primitive Types (e.g., numbers, strings): Functions mein by value pass hoti hain, matlab function ke andar ki changes asal variable par asar nahi karte.
+
+//Objects/Arrays: Functions mein by reference pass hoti hain, matlab function ke andar ki changes asal object ya array par asar karti hain.
+
+function myCars(obj) {
+
+    return obj.color = "gray";
+
+
+
+}
+
+const cars = {
+    make: "honda",
+    model: 2009,
+    color: "black",
+
+};
+
+const e = myCars(cars);
+
+console.log(e);
